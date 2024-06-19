6f306f10d9b015b2e650f3ae5880f6d53bb0da1d
stevenFrozt <90814089+stevenFrozt@users.noreply.github.com>
2024-06-19 21:32:32 +0800

    test

--- src/components/custom/Navbar.tsx
+++ src/components/custom/Navbar.tsx
@@ -1,30 +1,72 @@
 "use client"
 import React from "react"
-
 import { Button } from "../ui/button"
 import Link from "next/link"
 import NavBurgerMobile from "./navBurgerMobile"
+import { usePathname } from "next/navigation"
+import { cn } from "@/lib/utils"
 
 export default function Navbar() {
+	const path = usePathname()
+
+	function IsLink(href: string) {
+		return path.endsWith(href)
+	}
+
 	return (
 		<nav className="flex justify-between items-center bg-white px-6 lg:px-28 py-4 lg:py-4 border">
 			<h3 className="font-medium text-black text-xl tracking-[.5rem]">
 				BINUATAN
 			</h3>
 			<div className="lg:flex gap-8 hidden">
-				<Button asChild variant={"link"} className="p-4 w-full">
+				<Button
+					asChild
+					variant={"link"}
+					className={cn(
+						"p-4 w-full",
+						IsLink("/") ? "text-orange-500" : ""
+					)}
+				>
 					<Link href={"/"}>Home</Link>
 				</Button>
-				<Button asChild variant={"link"} className="p-4 w-full">
+				<Button
+					asChild
+					variant={"link"}
+					className={cn(
+						"p-4 w-full",
+						IsLink("products") ? "text-orange-500" : ""
+					)}
+				>
 					<Link href={"products"}>Products</Link>
 				</Button>
-				<Button asChild variant={"link"} className="p-4 w-full">
+				<Button
+					asChild
+					variant={"link"}
+					className={cn(
+						"p-4 w-full",
+						IsLink("about") ? "text-orange-500" : ""
+					)}
+				>
 					<Link href={"about"}>About Us</Link>
 				</Button>
-				<Button asChild variant={"link"} className="p-4 w-full">
+				<Button
+					asChild
+					variant={"link"}
+					className={cn(
+						"p-4 w-full",
+						IsLink("news") ? "text-orange-500" : ""
+					)}
+				>
 					<Link href={"news"}>News</Link>
 				</Button>
-				<Button asChild variant={"link"} className="p-4 w-full">
+				<Button
+					asChild
+					variant={"link"}
+					className={cn(
+						"p-4 w-full",
+						IsLink("contact") ? "text-orange-500" : ""
+					)}
+				>
 					<Link href={"contact"}>Contact Us</Link>
 				</Button>
 			</div>
